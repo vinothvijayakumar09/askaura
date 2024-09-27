621e32b510894c474617ea505dcb015167ae1a01
vinothvijayakumar09 <vinoth.vijayakumar09@outlook.com>
2024-09-27 22:22:48 +0530

    first v1

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+// Your JavaScript code here, if needed
+// You can add any interactive features for your site
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Document loaded. Ready to interact!");
+});
